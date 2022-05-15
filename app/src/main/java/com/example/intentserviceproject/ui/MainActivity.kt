d3a70bee6e818c2dff24a4f7d4635c9c61af727d
lekoal <coreglus@gmail.com>
2022-05-15 14:35:40 +0300

    Buttons & progress bar are added

--- app/src/main/java/com/example/intentserviceproject/ui/MainActivity.kt
+++ app/src/main/java/com/example/intentserviceproject/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.intentserviceproject
+package com.example.intentserviceproject.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.intentserviceproject.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
